cd1a1def140dc3fb9848d5460ccda05ef757c94a
Daria Kreydina <darikre@users.noreply.github.com>
2018-10-01 11:52:32 +0300

    Viies tund

---
+++ Tund_5/index_5.php
@@ -0,0 +1,22 @@
+<?php
+  
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <title>
+  
+</head>
+<body>
+  <h1>Teretulemast</h1>
+  <p>Siin on minu <a href ="http://www.tlu.ee" target="_blank"> TLÜ </a> õppetöö raames valminud veebilehed. Need ei oma mingit sügavat sisu ja nende kopeerimine ei oma mõtet.</p>
+  <P><u>See lehekülg on ainult harjutamiseks.</u></p>
+  <hr>
+  <p><a href="new user.php">Loo kasutaja!</a></p>
+  
+ 
+</body>
+</html>
