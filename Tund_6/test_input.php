af9ef31f255e9d65ec2fadbdeb9e6bba453231f7
Daria Kreydina <darikre@users.noreply.github.com>
2018-10-08 11:32:55 +0300

    Kuuenda tunni materjal

---
+++ Tund_6/test_input.php
@@ -0,0 +1,19 @@
+ <?php
+  require("functions.php");
+  $notice = addcat();
+?>
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Tabel"Kiisu"</title>
+</head>
+<body>
+  <h1>Andmed</h1>
+  
+  <hr>
+  
+  <?php echo $notice; ?>
+ </body>
+</html>
+  
